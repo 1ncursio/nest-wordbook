2fd38e38b3fd2a8b6f740546ae8946ff54d95315
1ncursio <ckswn1323@naver.com>
2021-07-05 23:11:43 +0900

    Refactor TypeORM config

---
+++ server/ormconfig.ts
@@ -0,0 +1,45 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import dotenv from 'dotenv';
+import { ExamWord } from './src/entities/exam-word.entity';
+import { Exam } from './src/entities/exam.entity';
+import { PartOfSpeech } from './src/entities/part-of-speech.entity';
+import { User } from './src/entities/user.entity';
+import { WordPartOfSpeech } from './src/entities/word-part-of-speech.entity';
+import { WordSense } from './src/entities/word-sense.entity';
+import { Word } from './src/entities/word.entity';
+import { WordbookSpaceMember } from './src/entities/wordbook-space-member.entity';
+import { WordbookSpaceRole } from './src/entities/wordbook-space-role.entity';
+import { WordbookSpace } from './src/entities/wordbook-space.entity';
+import { Wordbook } from './src/entities/wordbook.entity';
+
+dotenv.config();
+const config: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [
+    User,
+    Wordbook,
+    Word,
+    Exam,
+    ExamWord,
+    WordSense,
+    PartOfSpeech,
+    WordPartOfSpeech,
+    WordbookSpace,
+    WordbookSpaceMember,
+    WordbookSpaceRole,
+  ],
+  migrations: [__dirname + '/./src/migrations/*.ts'],
+  cli: { migrationsDir: './src/migrations' },
+  autoLoadEntities: true,
+  charset: 'utf8mb4',
+  synchronize: false,
+  logging: true,
+  keepConnectionAlive: true,
+};
+
+export = config;
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,53 +1,20 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as ormconfig from 'ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { Exam } from './entities/exam.entity';
-import { ExamWord } from './entities/exam-word.entity';
-import { PartOfSpeech } from './entities/part-of-speech.entity';
-import { User } from './entities/user.entity';
-import { Wordbook } from './entities/wordbook.entity';
-import { WordPartOfSpeech } from './entities/word-part-of-speech.entity';
-import { Word } from './entities/word.entity';
-import { WordSense } from './entities/word-sense.entity';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { UsersModule } from './users/users.module';
 import { WordbooksModule } from './wordbooks/wordbooks.module';
-import { WordsModule } from './words/words.module';
-import { WordbookSpace } from './entities/wordbook-space.entity';
-import { WordbookSpaceMember } from './entities/wordbook-space-member.entity';
-import { WordbookSpaceRole } from './entities/wordbook-space-role.entity';
 import { WordbookspacesModule } from './wordbookspaces/wordbook-spaces.module';
+import { WordsModule } from './words/words.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, cache: true }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [
-        User,
-        Wordbook,
-        Word,
-        Exam,
-        ExamWord,
-        WordSense,
-        PartOfSpeech,
-        WordPartOfSpeech,
-        WordbookSpace,
-        WordbookSpaceMember,
-        WordbookSpaceRole,
-      ],
-      synchronize: process.env.NODE_ENV !== 'production',
-      logging: true,
-      keepConnectionAlive: true,
-    }),
+    TypeOrmModule.forRoot(ormconfig),
     AuthModule,
     UsersModule,
     WordbooksModule,
