91412a5a017ee8ae75628cfc3806eecf4055caee
1ncursio <ckswn1323@naver.com>
2021-07-13 22:13:20 +0900

    Rename JoinGoogleUserDto to JoinOAuthUserDto

--- server/src/users/dto/join-google-user.dto.ts
+++ server/src/users/dto/join-google-user.dto.ts
@@ -1,7 +1,7 @@
 import { PickType } from '@nestjs/swagger';
 import { User } from 'src/entities/user.entity';
 
-export class JoinGoogleUserDto extends PickType(User, [
+export class JoinOAuthUserDto extends PickType(User, [
   'email',
   'username',
   'image',
