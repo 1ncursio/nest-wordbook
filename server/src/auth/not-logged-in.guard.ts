4e14b95ca975f4b94bbb8caf412ced19901afe9f
1ncursio <ckswn1323@naver.com>
2021-07-13 22:09:21 +0900

    Modify login check guards

--- server/src/auth/not-logged-in.guard.ts
+++ server/src/auth/not-logged-in.guard.ts
@@ -1,4 +1,10 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
 import { Observable } from 'rxjs';
 
 @Injectable()
@@ -6,7 +12,11 @@ export class NotLoggedInGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const request = context.switchToHttp().getRequest();
-    return !request.isAuthenticated();
+    const request = context.switchToHttp().getRequest<Request>();
+    if (request?.cookies?.Authentication || request?.cookies?.Refresh) {
+      throw new UnauthorizedException('로그인한 유저는 접근할 수 없습니다.');
+    }
+
+    return true;
   }
 }
