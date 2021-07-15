15df8bb4eb90ca71b030e2d7c8e7cfdce6317644
1ncursio <ckswn1323@naver.com>
2021-07-16 03:23:21 +0900

    Add button

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Login = () => {
-  return <div className="w-full h-full flex">로그인</div>;
+  return (
+    <div className="w-full h-full flex">
+      <button className="bg-blue-500 text-gray-100 rounded-md py-2 px-4 ">ㅅㅂ</button>
+    </div>
+  );
 };
 
 export default Login;
