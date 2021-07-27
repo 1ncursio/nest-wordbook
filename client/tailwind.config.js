e4e89f703ea814cf0c7e04410661912330ef041f
1ncursio <ckswn1323@naver.com>
2021-07-28 05:03:12 +0900

    Modify tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
       flex: {
         2: '2 2 0%',
       },
+      boxShadow: {
+        DEFAULT:
+          '0 0 20px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+      },
     },
     borderWidth: {
       DEFAULT: '1px',
