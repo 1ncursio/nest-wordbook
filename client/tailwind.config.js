c012d4da3ec7df9e5f0a460fe52510ce547fc277
1ncursio <ckswn1323@naver.com>
2021-08-05 00:37:52 +0900

    Modify tailwind screens custom theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -43,7 +43,11 @@ module.exports = {
       github: '#3C4043',
     },
     screens: {
+      '2xl': { max: '1535px' },
+      xl: { max: '1279px' },
+      lg: { max: '1023px' },
       md: { max: '767px' },
+      sm: { max: '639px' },
     },
   },
   variants: {
