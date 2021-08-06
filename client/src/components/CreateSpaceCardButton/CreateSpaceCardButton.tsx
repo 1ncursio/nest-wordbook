5e07ab965f04a839403c78c5cc030922637fb706
1ncursio <ckswn1323@naver.com>
2021-08-06 20:49:22 +0900

    Modify CreateSpaceCardButton component

--- client/src/components/CreateSpaceCardButton/CreateSpaceCardButton.tsx
+++ client/src/components/CreateSpaceCardButton/CreateSpaceCardButton.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useCallback, useState, VFC } from 'react';
 import Icon from '../Icon';
+import StyledModal from '../StyledModal';
 
-const CreateSpaceCardButton = () => {
+const CreateSpaceCardButton: VFC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = useCallback(() => {
+    setIsOpen(true);
+  }, []);
+
+  const closeModal = useCallback(() => {
+    setIsOpen(false);
+  }, []);
+
   return (
     <>
-      <button className="flex flex-col sm:flex-row justify-center items-center rounded-xl shadow transform transition hover:scale-105 hover:shadow-lg bg-white border-3 sm:border sm:h-16 border-cyan-600 border-dashed sm:border-solid">
+      <button
+        type="button"
+        onClick={openModal}
+        className="flex flex-col sm:flex-row justify-center items-center rounded-xl shadow transform transition hover:scale-105 hover:shadow-lg bg-white border-3 sm:border sm:h-16 border-cyan-600 border-dashed sm:border-solid"
+      >
         <Icon name="plus" className="text-cyan-600 w-16 h-16 sm:w-8 sm:h-8" />
         <span className="text-lg font-bold text-gray-600">새로 만들기</span>
       </button>
+      <StyledModal isOpen={isOpen} onRequestClose={closeModal} title="타이틀">
+        gd
+      </StyledModal>
     </>
   );
 };
--- client/src/components/SpaceCard/SpaceCard.tsx
+++ client/src/components/SpaceCard/SpaceCard.tsx
@@ -1,48 +1,58 @@
-import React, { useMemo } from 'react';
+import React, { FC, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import { spaceThumbnail, userThumbnail } from '../../assets/images';
 import { WordbookSpace } from '../../lib/api/typings/wordbookspace';
 import optimizeImage from '../../lib/optimizeImage';
+import CreateSpaceCardButton from '../CreateSpaceCardButton';
 
 export type SpaceCardProps = {
-  space: WordbookSpace;
+  spaces: WordbookSpace[];
 };
 
-const SpaceCard = ({ space }: SpaceCardProps) => {
-  const shortBio = useMemo(
-    () =>
-      space.shortBio!.length >= 80
-        ? `${space.shortBio!.slice(0, 80)}...`
-        : space.shortBio,
-    [space],
+const SpaceCard: FC<SpaceCardProps> = ({ spaces }) => {
+  const ellipsisShortBio = useMemo(
+    () => (shortBio: string) =>
+      shortBio.length >= 80 ? `${shortBio.slice(0, 80)}...` : shortBio,
+    [],
   );
 
   return (
-    <article className="flex flex-col rounded-xl shadow transform transition hover:scale-105 hover:shadow-lg bg-white">
-      <Link to={`/wordbookspaces/${space.id}`}>
-        <header className="px-4 py-2">
-          <h3 className="font-bold text-gray-800 text-xl">{space.name}</h3>
-        </header>
-        <div className="aspect-w-16 aspect-h-9">
-          <img
-            src={optimizeImage(space.image ?? spaceThumbnail)}
-            alt="space-thumbnail"
-            className="w-full h-full object-center object-cover"
-          />
-        </div>
-        <div className="p-4 h-16 overflow-ellipsis overflow-hidden">
-          <p className="text-gray-600 text-sm line-clamp-2">{shortBio}</p>
-        </div>
-      </Link>
-      <footer className="flex items-center p-4">
-        <img
-          src={optimizeImage(space.Owner.image ?? userThumbnail)}
-          alt="profile"
-          className="rounded-full w-6 h-6 object-cover mr-2"
-        />
-        <b className="text-sm">{space.Owner.username}</b>
-      </footer>
-    </article>
+    <>
+      <CreateSpaceCardButton />
+      {React.Children.toArray(
+        spaces.map((space) => (
+          <article className="flex flex-col rounded-xl shadow transform transition hover:scale-105 hover:shadow-lg bg-white">
+            <Link to={`/wordbookspaces/${space.id}`}>
+              <header className="px-4 py-2">
+                <h3 className="font-bold text-gray-800 text-xl">
+                  {space.name}
+                </h3>
+              </header>
+              <div className="aspect-w-16 aspect-h-9">
+                <img
+                  src={optimizeImage(space.image ?? spaceThumbnail)}
+                  alt="space-thumbnail"
+                  className="w-full h-full object-center object-cover"
+                />
+              </div>
+              <div className="p-4 h-16 overflow-ellipsis overflow-hidden">
+                <p className="text-gray-600 text-sm line-clamp-2">
+                  {ellipsisShortBio(space.shortBio ?? '')}
+                </p>
+              </div>
+            </Link>
+            <footer className="flex items-center p-4">
+              <img
+                src={optimizeImage(space.Owner.image ?? userThumbnail)}
+                alt="profile"
+                className="rounded-full w-6 h-6 object-cover mr-2"
+              />
+              <b className="text-sm">{space.Owner.username}</b>
+            </footer>
+          </article>
+        )),
+      )}
+    </>
   );
 };
 
