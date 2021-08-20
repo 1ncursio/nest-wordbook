625e9d607cdc9f6766c773faaee1944fdee25a9a
1ncursio <ckswn1323@naver.com>
2021-08-20 20:50:40 +0900

    Add examples

---
+++ examples/pos.js
@@ -0,0 +1,191 @@
+名詞;
+
+/* Morphemes representing concrete object such as a person, a place, etc.
+
+- */
+
+名詞接尾辞;
+
+/* Morpheme that comes after noun
+
+「組」 in 「f組」, 「性」 in 「可用性」 */
+
+冠名詞;
+
+/* Morpheme that comes before noun
+
+「大」 in 「大募集」 */
+
+英語接尾辞;
+
+/* Morpheme that comes before noun
+
+“ing” in “talking” */
+
+動詞語幹;
+
+/* The morpheme that comes after English noun
+
+「表」 in 「表す」, 「見え」 in 「見える」 */
+
+動詞活用語尾;
+
+/* Morphemes representing verb stem
+
+「し」 in 「示している」 */
+
+動詞接尾辞;
+
+/* Morpheme that come after verb stem, representing the verb conjugation. (Conjugated verb is a verb that has been altered from its base form.)
+
+「て」 in 「示している」 */
+
+冠動詞;
+
+/* Morphemes that comes after verb stem, and verb conjugations
+
+「ぶち」 in 「ぶち破る」 */
+
+補助名詞;
+
+/* Morphemes that comes before verb stem
+
+「ため」 in 「顔に出たため」, 「こと」 in 「見なかったこと」 */
+
+形容詞語幹;
+
+/* Morphemes that appear right after adnominal phrases
+
+「安」 in 「安い価格」 */
+
+形容詞接尾辞;
+
+/* Morphemes representing adjective stem
+
+「い」 in 「安い価格」 */
+
+冠形容詞;
+
+/* Morphemes that come after adjective stem
+
+「同じ」 in 「同じ役割」 */
+
+連体詞;
+
+/* Morphemes that come before adjective stem
+
+「同じ役割」の「同じ」 */
+
+連用詞;
+
+/* Morphemes that constitute an attributive clause independently
+
+「また」 in 「また同様に」 */
+
+接続詞;
+
+/* Morphemes that constitute conjunctive clause independently, which is commonly called "adverb".
+
+「しかし」「また」「なお」 */
+
+独立詞;
+
+/* Morphemes that comes at the beginning of sentence, and expressing relations between two sentences.
+
+「さようなら」 */
+
+接続接尾辞;
+
+/* Morphemes that express greetings and addressing, also called "interjection".
+
+「が」 in 「～であるが」 */
+
+判定詞;
+
+/* Morphemes that come after verb and adjective
+
+「では」 in 「ニューデリーでは」 */
+
+格助詞;
+
+/* Morphemes that constitute the clause of predicate that comes after the noun
+
+「が」 in 「リンゴが赤い」 */
+
+引用助詞;
+
+/* Morphemes that constitute conjunctive and adnominal, that immediately follow the modified noun, verb, adjective, or sentence.
+
+「と」 in 「～だと推測される」 */
+
+連用助詞;
+
+/* Morphemes that express quotes
+
+「も」 in 「現在も続く」 */
+
+終助詞;
+
+/* Morphemes that also called as predicate
+
+「よ」 in 「～ですよ」 */
+
+間投詞;
+
+/* Morphemes that terminate the sentence.
+
+「ね」 in 「私はね、～」 */
+
+括弧;
+
+/* Parentheses and brackets.
+
+- */
+
+句点;
+
+/* Morphemes consists of punctuation marks such are period, question mark, and exclamation point.
+
+- */
+
+読点;
+
+/* Comma
+
+- */
+
+空白;
+
+/* Space
+
+- */
+
+Symbol;
+
+/* Morphemes of symbol
+
+“♪”, “+”, etc. */
+
+Number;
+
+/* Morphemes that represent numerical value.
+
+- */
+
+助数詞;
+
+/* Morphemes that follow the number
+
+「杯」 in 「約3杯」 */
+
+助助数詞;
+
+/* Morphemes that follow counter suffix
+
+「あたり」 in 「五個あたり」 */
+
+冠数詞;
+
+/* Morphemes that comes before the number
+
+「第」 in 「第3回目」 */
