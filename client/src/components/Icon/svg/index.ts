61678c666b4cabb7ed318f92cc6e78972d693aa0
1ncursio <ckswn1323@naver.com>
2021-08-27 16:45:45 +0900

    Add icons

--- client/src/components/Icon/svg/index.ts
+++ client/src/components/Icon/svg/index.ts
@@ -1,6 +1,10 @@
+import { ReactComponent as download } from './download.svg';
 import { ReactComponent as github } from './github.svg';
 import { ReactComponent as google } from './google.svg';
 import { ReactComponent as kakao } from './kakao.svg';
 import { ReactComponent as plus } from './plus.svg';
+import { ReactComponent as remove } from './remove.svg';
+import { ReactComponent as reorder } from './reorder.svg';
+import { ReactComponent as upload } from './upload.svg';
 
-export { github, google, kakao, plus };
+export { github, google, kakao, plus, remove, reorder, upload, download };
